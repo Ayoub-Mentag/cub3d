ad52547c06d9d221dfd0cba1b633bbbc4b281627
Ayoub Mentag <amentag@e1r5p3.1337.ma>
2023-05-13 21:09:14 +0100

    second

--- main.c
+++ main.c
@@ -26,49 +26,8 @@ void	my_mlx_pixel_put(t_all *all, int x, int y, int color)
 	*(unsigned int*)dst = color;
 }
 
-int    draw_map(t_all *all)
+void    draw_line(t_all *all)
 {
-    //update player
-	all->player->rotationAngle += all->player->turnDirection * all->player->rotationSpeed;
-	int moveStep = all->player->walkDirection * all->player->moveSpeed;
-	double newPlayerX = all->player->coor.x + cos(all->player->rotationAngle) * moveStep;
-    double newPlayerY = all->player->coor.y + sin(all->player->rotationAngle) * moveStep;
-    // printf("old(%d, %d); new(%f, %f)\n", all->player->coor.x, all->player->coor.y, newPlayerX, newPlayerY);
-	all->player->coor.x = newPlayerX;
-	all->player->coor.y = newPlayerY;
-
-	mlx_put_image_to_window(all->mlx, all->win, all->data.img, 0, 0);
-    int d1;
-    int d2;
-    int color;
-
-    for (int i = 0; i < WINDOW_HEIGHT; i++)
-    {
-        for (int j = 0; j < 640; j++)
-        {
-            // printf("number -----> %d\n", k++);
-			d1 = i / 32;
-			d2 = j / 32;
-            // if (d1 < 15 && d2 < 15 && grid[d1][d2] == 2)
-            // {
-            //     player->playerX = d1 * 32;
-            //     player->playerY = d2 * 32;
-            // }
-			color = d1 >= 15 || d2 >= 15 || grid[d1][d2] == 1 ? 0x000000 : 0xFFFFFF;
-			my_mlx_pixel_put(all, j, i, color);
-		}
-	}
-
-	color = 0x0000FF;
-	// // //draw player
-	for (int i = 0; i < all->player->side; i++)
-	{
-		for (int j = 0; j < all->player->side; j++)
-		{
-			my_mlx_pixel_put(all, all->player->coor.x + j, all->player->coor.y + i, 0x00FF0000);
-		}
-    }
-
     int side = all->player->side;
     // //draw line
     t_point a, b;
@@ -98,7 +57,56 @@ int    draw_map(t_all *all)
         x += x_increment;
         y += y_increment;
     }
+}
 
+void    draw_player(t_all *all)
+{
+    for (int i = 0; i < all->player->side; i++)
+	{
+		for (int j = 0; j < all->player->side; j++)
+		{
+			my_mlx_pixel_put(all, all->player->coor.x + j, all->player->coor.y + i, 0x00FF0000);
+		}
+    }
+}
+
+
+void    update_coordination(t_all *all)
+{
+	all->player->rotationAngle += all->player->turnDirection * all->player->rotationSpeed;
+	int moveStep = all->player->walkDirection * all->player->moveSpeed;
+	double newPlayerX = all->player->coor.x + cos(all->player->rotationAngle) * moveStep;
+    double newPlayerY = all->player->coor.y + sin(all->player->rotationAngle) * moveStep;
+	all->player->coor.x = newPlayerX;
+	all->player->coor.y = newPlayerY;
+}
+
+int    draw_map(t_all *all)
+{
+    int d1;
+    int d2;
+    int color;
+
+    update_coordination(all);
+	mlx_put_image_to_window(all->mlx, all->win, all->data.img, 0, 0);
+    for (int i = 0; i < WINDOW_HEIGHT; i++)
+    {
+        for (int j = 0; j < 640; j++)
+        {
+			d1 = i / 32;
+			d2 = j / 32;
+            // if (d1 < 15 && d2 < 15 && grid[d1][d2] == 2)
+            // {
+            //     player->playerX = d1 * 32;
+            //     player->playerY = d2 * 32;
+            // }
+			color = d1 >= 15 || d2 >= 15 || grid[d1][d2] == 1 ? 0x000000 : 0xFFFFFF;
+			my_mlx_pixel_put(all, j, i, color);
+		}
+	}
+
+    draw_player(all);
+    draw_line(all);
     return (0);
 }
 
@@ -128,6 +136,9 @@ t_all   *init_all()
     all->player = player;
     all->mlx = mlx;
     all->win = win;
+
+    all->data.img = mlx_new_image(all->mlx, WINDOW_WIDTH, WINDOW_HEIGHT);
+	all->data.addr = mlx_get_data_addr(all->data.img, &all->data.bits_per_pixel, &all->data.line_length,&all->data.endian);
     return (all);
 }
 
@@ -175,8 +186,6 @@ int	key_released(int key, void *p)
 int main()
 {
     t_all   *all = init_all();
-    all->data.img = mlx_new_image(all->mlx, WINDOW_WIDTH, WINDOW_HEIGHT);
-	all->data.addr = mlx_get_data_addr(all->data.img, &all->data.bits_per_pixel, &all->data.line_length,&all->data.endian);
 
     mlx_hook(all->win, 2, 0, key_hook, all);
     mlx_hook(all->win, 3, 0, key_released, all);
